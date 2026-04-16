17925f00be36fa5dc0d33eee937c4678de479058
Kelvin Vaz <kelvinvass12@gmail.com>
2026-04-16 19:57:36 -0300

    ajuste views cadastro

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -5,7 +5,8 @@ from rest_framework import status
 from .models import User
 from .serializers import UserSerializer
 
-# 1. REGISTRO (Público)
+# views.py
+
 @api_view(['POST'])
 @authentication_classes([]) 
 @permission_classes([AllowAny]) 
@@ -13,33 +14,25 @@ def registrar_usuario(request):
     data = request.data
     try:
         if User.objects.filter(username=data.get('username')).exists():
-            return Response({"error": "Usuário já existe"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": "Nome de usuário já está em uso."}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Cuidado com campos nulos na data de nascimento
+        data_nasc = data.get('data_nascimento')
+        if not data_nasc:
+            data_nasc = None
 
         user = User.objects.create_user(
             username=data.get('username'),
             password=data.get('password'),
             email=data.get('email'),
-            first_name=data.get('nome', ''), # Salva o nome completo aqui
+            first_name=data.get('nome', ''),
             role=data.get('role', 'IDOSO'),
-            data_nascimento=data.get('data_nascimento'),
-            telefone=data.get('telefone') # TELEFONE ADICIONADO NO REGISTRO
+            data_nascimento=data_nasc,
+            telefone=data.get('telefone'),
+            cns=data.get('cns') # <--- FALTAVA ISSO AQUI
         )
         return Response({"message": "Usuário criado com sucesso!"}, status=status.HTTP_201_CREATED)
     except Exception as e:
+        # Retorna o erro real para o console do celular
+        print(f"Erro no registro: {str(e)}")
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-# 2. PERFIL (Privado - PATCH)
-@api_view(['GET', 'PATCH'])
-@permission_classes([IsAuthenticated])
-def gerenciar_perfil(request):
-    user = request.user
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-    
-    elif request.method == 'PATCH':
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

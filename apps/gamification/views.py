6b6af3ad30a3ad13740e8a3d42298eb0707c8e12
Kelvin Vaz <kelvinvass12@gmail.com>
2026-04-17 01:20:26 -0300

    asdfg

--- apps/gamification/views.py
+++ apps/gamification/views.py
@@ -11,12 +11,15 @@ class PontuacaoViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
     def list(self, request, *args, **kwargs):
-    # O .values() cria chaves como 'usuario__username'
-        ranking = Pontuacao.objects.values(
-            'usuario__username', 
-            'usuario__perfil' 
-        ).annotate(
-            total_pontos=Sum('pontos')
-        ).order_by('-total_pontos')
-        
-        return Response(list(ranking), status=status.HTTP_200_OK)
+        try:
+            ranking = Pontuacao.objects.values(
+                'usuario__username' # Deixe apenas campos que existem no User
+            ).annotate(
+                total_pontos=Sum('pontos')
+            ).order_by('-total_pontos')
+            
+            return Response(list(ranking), status=status.HTTP_200_OK)
+        except Exception as e:
+            # Isso vai te ajudar a ver o erro real no log do Render/Terminal
+            print(f"ERRO NO RANKING: {e}") 
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)

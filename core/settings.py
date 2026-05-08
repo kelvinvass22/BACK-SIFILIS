2dc64ed31feb571059e2dd7cdc7218fdea2f286b
Kelvin Vaz <kelvinvass12@gmail.com>
2026-05-07 21:45:12 -0300

    afaaaa

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.resend.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True  
 
 # No Resend, o usuário é fixo, o que muda é a senha (API KEY)
 EMAIL_HOST_USER = 'resend'
